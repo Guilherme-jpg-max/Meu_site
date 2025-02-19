7b35db3bf8ef24fb1d38975001ed0c21ce06500b
Guilherme-jpg-max <guicasii5@gmail.com>
2025-02-18 22:22:27 -0300

    Preparando projeto para subir para o render

--- app.py
+++ app.py
@@ -5,10 +5,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-
     tempo_desde_conhecimento = calcular_tempo()
     legendas = obter_legendas()
     return render_template('index.html', tempo=tempo_desde_conhecimento, legendas=legendas)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
